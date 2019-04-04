efc59cc000022153282377c48a3f3b89020da3e0
richietam <richardtam.ny@gmail.com>
2019-04-03 23:06:00 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded")
+
+  const idText = document.querySelector("#text")
+  idText.innerHTML = "This is really cool!"
+
+})
